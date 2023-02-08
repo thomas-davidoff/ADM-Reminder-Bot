471a571d421f93eb026176bf1d0ba9ad60fcfd81
Thomas Davidoff <68920095+thomasdavidoff@users.noreply.github.com>
2023-02-08 16:19:34 -0500

    Added conventions, clarified dancer reference, fixed forgotten unresolved reference since last edit

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from email.mime.multipart import MIMEMultipart
 
 
 def create_html(appt, temp):
-    dancer = appt['dancer']
+    person = appt['dancer']
     pt = appt['pt']
     time = appt['datetime']
     relative_time = appt['relative_time']
 
-    render = temp.render(dancer=dancer, pt=pt.capitalize(), appt_time=time, relative_time=relative_time)
+    render = temp.render(dancer=person, pt=pt.capitalize(), appt_time=time, relative_time=relative_time)
     return render
 
 
@@ -35,27 +35,27 @@ def send_mail(service, message):
     return send_message
 
 
-template = Template(filename='email_template.html')
-with open('emails.json') as f:
-    email_dict = json.load(f)
-    email_dict = {k.lower(): v for k, v in email_dict.items()}
+if __name__ == '__main__':
+    template = Template(filename='email_template.html')
+    with open('emails.json') as f:
+        email_dict = json.load(f)
+        email_dict = {k.lower(): v for k, v in email_dict.items()}
 
+    example_appt = [{
+        'dancer': 'Thomas',
+        'pt': 'ANN',
+        'datetime': 'Thursday, Feb 9 at 4:50 PM',
+        'relative_time': 'tomorrow'
+    }]
 
-# example_appt = [{
-#     'dancer': 'Thomas',
-#     'pt': 'ANN',
-#     'datetime': 'Thursday, Feb 9 at 4:50 PM',
-#     'relative_time': 'tomorrow'
-# }]
-#
-# next_appointments = example_appt
+    next_appointments = example_appt
 
-for appointment in next_appointments:
-    email_render = create_html(appointment, template)
-    dancer = appointment['dancer'].lower().strip()
-    if dancer in email_dict:
-        print(f'Sending to {dancer} at {email_dict[dancer]}...')
-        finished_email = create_message(send_to, email_render, appointment['datetime'])
-        send_mail(gmail_service, finished_email)
-    else:
-        print(f'Could not find {dancer} in email file.')
+    for appointment in next_appointments:
+        email_render = create_html(appointment, template)
+        dancer = appointment['dancer'].lower().strip()
+        if dancer in email_dict:
+            print(f'Sending to {dancer} at {email_dict[dancer]}...')
+            finished_email = create_message(email_dict[dancer], email_render, appointment['datetime'])
+            send_mail(gmail_service, finished_email)
+        else:
+            print(f'Could not find {dancer} in email file.')
